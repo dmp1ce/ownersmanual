128d6a2267b75a56172b106de4b2ade48b62f849
David Parrish <daveparrish@gmail.com>
2013-08-23 19:51:52 -0400

    Enable filtering of foods based on a search filter.  Properly filters the headers as well as the food items.

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,7 +1,30 @@
-function FoodListCtrl($scope, $http) {
+function FoodListCtrl($scope, $http, $filter) {
   $http.get("js/foods.json").success(function(data) {
     $scope.foods = data;
+
+    $scope.green_greens = $filter('filter')(data, {color:"Green-Green"});
+    $scope.greens = $filter('filter')(data, function(food) {
+      if (food.color == "Green") { return true; }
+    });
+    $scope.green_yellows = $filter('filter')(data, {color:"Green-Yellow"});
+    $scope.yellows = $filter('filter')(data, function(food) {
+      if (food.color == "Yellow") { return true; }
+    });
+    $scope.yellow_reds = $filter('filter')(data, {color:"Yellow-Red"});
+    $scope.reds = $filter('filter')(data, function(food) {
+      if (food.color == "Red") { return true; }
+    });
+
+    //$scope.green_greens = FilterFoodCategories(green_greens, $filter)
   });
   
   $scope.orderProp = "color";
 }
+
+//function FilterFoodCategories(foods, $filter) {
+//  var filtered = new Object();
+//  filtered.veg_non_starchy = $filter('filter')(foods, {category:"Vegetable:Non-Starchy"});
+//  filtered.herbs = $filter('filter')(foods, {category:"Herbs"});
+//
+//  return filtered;
+//}
